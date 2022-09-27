99dda5420abaf8143ea02ded2ee01438457dcac7
Denys Aksonov <109756577+reactDenys@users.noreply.github.com>
2022-09-27 16:34:14 +0300

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -1,14 +1,19 @@
-const articlesSwiper = new Swiper('.articles', {
+const watches_section = document.querySelector('.watches_section')
+const watches__wrapper = document.querySelector('.watches__wrapper')
+const watch__items = document.querySelectorAll('.watch__item')
+const prev  = document.querySelector('.watches__swiper-button-prev')
+const next = document.querySelector('.watches__swiper-button-next')
+
+new Swiper('.articles', {
     navigation: {
         prevEl: '.swiper-button-prev',
         nextEl: '.swiper-button-next',
     }
 });
-const watches_section = document.querySelector('.watches_section')
-const watches__wrapper = document.querySelector('.watches__wrapper')
-const watch__items = document.querySelectorAll('.watch__item')
 
 if (window.innerWidth <= 770 ) {
+    prev.classList.add('swiper-button-prev')
+    next.classList.add('swiper-button-next')
     watches_section.classList.add('swiper')
     watches__wrapper.classList.add('swiper-wrapper')
     watch__items.forEach( el => {el.classList.add('swiper-slide')})
@@ -18,8 +23,4 @@ if (window.innerWidth <= 770 ) {
             nextEl: '.swiper-button-next',
         }
     });
-} else {
-    watches_section.classList.remove('swiper')
-    watches__wrapper.classList.remove('swiper-wrapper')
-    watch__items.forEach( el => {el.classList.remove('swiper-slide')})
-};
+}
