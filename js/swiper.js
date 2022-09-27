3e3ccd039aed0f59f20fb553ed695865ef7e7945
Denys Aksonov <109756577+reactDenys@users.noreply.github.com>
2022-09-27 16:58:00 +0300

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -11,16 +11,29 @@ new Swiper('.articles', {
     }
 });
 
-if (window.innerWidth <= 770 ) {
-    prev.classList.add('swiper-button-prev')
-    next.classList.add('swiper-button-next')
-    watches_section.classList.add('swiper')
-    watches__wrapper.classList.add('swiper-wrapper')
-    watch__items.forEach( el => {el.classList.add('swiper-slide')})
-    new Swiper('.watches_section', {
-        navigation: {
-            prevEl: '.swiper-button-prev',
-            nextEl: '.swiper-button-next',
-        }
-    });
-}
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 770) {
+        prev.classList.add('swiper-button-prev')
+        next.classList.add('swiper-button-next')
+        watches_section.classList.add('swiper')
+        watches__wrapper.classList.add('swiper-wrapper')
+        watch__items.forEach(el => {
+            el.classList.add('swiper-slide')
+        })
+        new Swiper('.watches_section', {
+            navigation: {
+                prevEl: '.swiper-button-prev',
+                nextEl: '.swiper-button-next',
+            }
+        });
+    } else {
+        prev.classList.remove('swiper-button-prev')
+        next.classList.remove('swiper-button-next')
+        watches_section.classList.remove('swiper')
+        watches__wrapper.classList.remove('swiper-wrapper')
+        watch__items.forEach(el => {
+            el.classList.remove('swiper-slide')
+            el.style.width = 'auto';
+        })
+    }
+});
